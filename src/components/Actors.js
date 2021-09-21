1db6e61d3f1c08244b30a12b99f07c1243e99fec
David Moina <davemolina777@gmail.com>
2021-09-21 11:05:12 -0500

    commit tree 973ef4efca4840e25c161ff7f67084c2204f38cf

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,17 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      { actors.map((actor) => (
+        <div className='actor' key={actor.name}>
+          <h3>{actor.name}</h3>
+          <ul>
+            {actor.movies.map((movie) => (
+              <li key={movie}>{ movie }</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,9 +4,18 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map((movie) => (
+          <div key={movie.name}>
+            {movie.title} <br />
+            {movie.time} <br />
+            <ul>
+              {movie.genres.map((gen) => <li key={gen}>{gen}</li> )}
+            </ul>
+          </div>
+        ))}
     </div>
-  );
-};
+  )
+}
 
 export default Movies;
